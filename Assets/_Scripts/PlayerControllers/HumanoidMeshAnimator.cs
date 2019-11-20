b806433fa2e2d5eb31abb87ea4d05fbb81430c45
Brian Landes <the.brian.landes@gmail.com>
2019-11-19 20:12:51 -0700

    Character mesh and animations (pretty rough)

---
+++ Assets/_Scripts/PlayerControllers/HumanoidMeshAnimator.cs
@@ -0,0 +1,93 @@
+﻿
+using UnityEngine;
+
+public class HumanoidMeshAnimator : MonoBehaviour {
+
+	public float forwardSpeedToAnimationScale = 8f;
+	public float sidewaysSpeedToAnimationScale = 8f;
+	public float turnSpeedToAnimationScale = 8f;
+	
+	public Grounded groundedComponent;
+	
+	public float maxUpperBodyWeight = 1f;
+	
+	Animator animator;
+
+	Vector3 lastPosition;
+	float lastRotationY = 0;
+
+	bool inLocomotion = false;
+
+	bool jumpBoosting = false;
+	
+	public int upperBodyLayerIndex = 2;
+
+	public float upperBodyWeightChangeScale = 0.1f;
+
+	private float targetUpperBodyWeight = 0f;
+	
+	private void Start() {
+		animator = GetComponent<Animator>();
+		lastPosition = transform.position;
+		lastRotationY = transform.rotation.eulerAngles.y;
+		
+	}
+	
+	public void ManualFireTrigger(string trigger) {
+		animator.SetTrigger(trigger);
+	}
+	
+	private void FixedUpdate() {
+		var velocity = transform.InverseTransformVector(GetVelocity().DropY());
+
+		float horizontalMovement = velocity.x;
+		float verticalMovement = velocity.z;
+		var currentTorque = GetTurnSpeed();
+
+		if ( Mathf.Approximately(0, horizontalMovement) && Mathf.Approximately(0, verticalMovement) && Mathf.Approximately(0, currentTorque)) {
+			if ( inLocomotion ) {
+				//animator.SetTrigger("StopLocomotion");
+				inLocomotion = false;
+			}
+		} else {
+			if (!inLocomotion) {
+				//animator.SetTrigger("StartLocomotion");
+				inLocomotion = true;
+			}
+			
+			//animator.SetFloat("TurnSpeed", currentTorque * turnSpeedToAnimationScale);
+
+			animator.SetFloat("ForwardSpeed", verticalMovement * forwardSpeedToAnimationScale);
+			animator.SetFloat("SidewaysSpeed", horizontalMovement * sidewaysSpeedToAnimationScale + currentTorque * turnSpeedToAnimationScale);
+		}
+		animator.SetBool("Running", inLocomotion);
+
+		//var currentTorque = GetTurnSpeed();
+		//animator.SetFloat("TurnSpeed", currentTorque * turnSpeedToAnimationScale);
+
+		lastPosition = transform.position;
+		lastRotationY = transform.rotation.eulerAngles.y;
+
+		//if (controller != null) {
+		//	animator.SetBool("Grounded", controller.IsGrounded);
+		//} else
+		if (groundedComponent != null) {
+			animator.SetBool("Grounded", groundedComponent.IsGrounded);
+		}
+
+
+		float currentWeight = animator.GetLayerWeight(upperBodyLayerIndex);
+		float difference = targetUpperBodyWeight - currentWeight;
+
+		animator.SetLayerWeight(upperBodyLayerIndex, currentWeight+ difference* upperBodyWeightChangeScale);
+	}
+	
+	private Vector3 GetVelocity() {
+		return transform.position - lastPosition;
+	}
+
+	private float GetTurnSpeed() {
+		return transform.rotation.eulerAngles.y - lastRotationY;
+	}
+	
+}
---
+++ Assets/_Scripts/PlayerControllers/TestAnimationScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TestAnimationScript : MonoBehaviour {
+	public Slider sidewaysSlider;
+	public Slider forwardsSlider;
+
+	public Toggle isRunningToggle;
+
+	public Animator animator;
+
+	private void LateUpdate() {
+		var verticalMovement = forwardsSlider.value;
+		var horizontalMovement = sidewaysSlider.value;
+
+		animator.SetFloat("ForwardSpeed", verticalMovement);
+		animator.SetFloat("SidewaysSpeed", horizontalMovement);
+
+		animator.SetBool("Running", isRunningToggle.isOn);
+	}
+}
